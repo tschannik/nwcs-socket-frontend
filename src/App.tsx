ae9f5b1844ba29e8f4e891f84725e71e25250b64
Yannik Zimmermann <yannik.z@me.com>
2021-05-30 13:27:39 +0200

    feat: remove sdx

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,8 @@ import { Route } from 'react-router-dom';
 
 import Home from './container/Home/Home';
 
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-import { JSX } from '@swisscom/sdx/dist/js/webcomponents/loader';
 import useSocket from './hooks/useSocket';
 import SocketContext from './hooks/Socket.context';
-type StencilProps<T> = {
-  [P in keyof T]?: Omit<T[P], 'ref'> | HTMLAttributes<T>;
-};
-type ReactProps<T> = {
-  [P in keyof T]?: DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>;
-};
-type StencilToReact<T = JSX.IntrinsicElements, U = HTMLElementTagNameMap> = StencilProps<T> &
-  ReactProps<U>;
-declare global {
-  export namespace JSX {
-    interface IntrinsicElements extends StencilToReact {}
-  }
-}
 
 function App() {
   const socket = useSocket();
